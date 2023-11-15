c5cd0ba9886a90b86f427f943fac96459fecf64a
tomiwadotJS <victorgbayedev@gmail.com>
2023-11-15 10:15:34 +0000

    dark mode alert

--- src/components/compound/Alert/Alert.jsx
+++ src/components/compound/Alert/Alert.jsx
@@ -5,9 +5,14 @@ import styles from './Alert.module.scss'
 import failureIcon from '../../../assets/failureIcon.svg'
 import successIcon from '../../../assets/successIcon.svg'
 import neutral from '../../../assets/neutral.svg'
+import infoIcon from '../../../assets/infoIcon.svg'
 import X from '../../../assets/X.svg'
+import darkX from '../../../assets/darkX.svg'
+import { useGlobalContext } from '../../../context';
 
 const Alert = ({ type, message, children }) => {
+  const {theme} = useGlobalContext()
+
     const [visible, setVisible] = useState(true);
 
   useEffect(() => {
@@ -26,22 +31,24 @@ const Alert = ({ type, message, children }) => {
   return (
         visible ?
         (
-            <div className={`${styles.alert} ${type === 'success' ? styles.success : type ==='failure' ? styles.failure : styles.neutral}`}>
+          <div className={` ${styles[theme]}`}>
+            <div className={`${styles.alert} ${type === 'success' ? styles.success : type ==='failure' ? styles.failure : type ==='info' ? styles.info : styles.neutral}`} >
                 {/* {type === 'success' ? <img src={successIcon}/> :  <img src={failureIcon}/>} */}
                 <div className={styles.alertContent}>
-                    <img src={type === 'success' ? successIcon : type ==='failure' ? failureIcon : neutral}/>
+                    <img src={type === 'success' ? successIcon : type ==='failure' ? failureIcon : type === 'info' ? infoIcon : neutral}/>
                     <div className={styles.AlertmessageStyle}>
                         <p className={styles.messageStyle}>{message}</p>
                         <p className={styles.child} style={{color:`${type === 'success'? '#448403':type ==='failure'? '#932221' : '#000'}`}}>{children}</p>
                     </div>
                 </div>
-                <img src={X} onClick={handleClose}/>
+                <img src={theme == 'dark' ? darkX : X} onClick={handleClose}/>
             </div>
+          </div>
         ) : null
   )
 }
 Alert.propTypes = {
-    type: PropTypes.oneOf(['success', 'failure', 'neutral']).isRequired,
+    type: PropTypes.oneOf(['success', 'failure', 'neutral, info']).isRequired,
     message: PropTypes.string,
     children: PropTypes.string,
   };
