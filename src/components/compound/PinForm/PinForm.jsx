c6052cb76b30a13ca89e837abfdaa23d543e101c
tomiwadotJS <victorgbayedev@gmail.com>
2023-12-13 14:08:32 +0000

    changes to input pin logic

--- src/components/compound/PinForm/PinForm.jsx
+++ src/components/compound/PinForm/PinForm.jsx
@@ -1,9 +1,71 @@
+// import { useState, useRef } from 'react';
+// import PropTypes from 'prop-types';
+// import { useGlobalContext } from '../../../context';
+// import styles from './PinForm.module.scss'
+// const PasscodeInput = ({ onSubmit }) => {
+//   const {theme} = useGlobalContext()
+//   const [passcode, setPasscode] = useState(['', '', '', '']);
+//   const inputRefs = [useRef(null), useRef(null), useRef(null), useRef(null)];
+
+//   const handleChange = (index, value) => {
+//     if (/^\d$/.test(value)) {
+//       const newPasscode = [...passcode];
+//       newPasscode[index] = value;
+//       setPasscode(newPasscode);
+//       if (index < 3 && value !== '') {
+//         inputRefs[index + 1].current.focus();
+//       }
+//       if (index === 3 && value !== '') {
+//         onSubmit(newPasscode.join(''));
+//       }
+//     } else if (value === '') {
+//       const newPasscode = [...passcode];
+//       newPasscode[index] = '';
+//       setPasscode(newPasscode);
+  
+//       // Allow navigating to the previous input field when deleting digits
+//       if (index > 0) {
+//         inputRefs[index - 1].current.focus();
+//       }
+//     }
+//   };
+
+//   return (
+//     <div
+//     style={{width:'100%', display:'flex', gap:'20px'}}
+//     >
+//       {passcode.map((digit, index) => (
+//         <input
+//           key={index}
+//           type="text"
+//           value={digit}
+//           maxLength="1"
+//           onChange={(e) => handleChange(index, e.target.value)}
+//           ref={inputRefs[index]}
+//           style={{height:'64px', padding:'8px 16px', width:'75px', borderRadius:'8px' }}
+//           className={`${styles.pinForm} ${styles[theme]}`}
+
+//         />
+//       ))}
+//     </div>
+//   );
+// };
+
+// PasscodeInput.propTypes = {
+//     onSubmit: PropTypes.func.isRequired,
+//   };
+
+// export default PasscodeInput;
+
+
+
 import { useState, useRef } from 'react';
 import PropTypes from 'prop-types';
 import { useGlobalContext } from '../../../context';
-import styles from './PinForm.module.scss'
+import styles from './PinForm.module.scss';
+
 const PasscodeInput = ({ onSubmit }) => {
-  const {theme} = useGlobalContext()
+  const { theme } = useGlobalContext();
   const [passcode, setPasscode] = useState(['', '', '', '']);
   const inputRefs = [useRef(null), useRef(null), useRef(null), useRef(null)];
 
@@ -18,16 +80,36 @@ const PasscodeInput = ({ onSubmit }) => {
       if (index === 3 && value !== '') {
         onSubmit(newPasscode.join(''));
       }
-    } else if (value === '' && index > 0) {
+    } else if (value === '') {
+      const newPasscode = [...passcode];
+      newPasscode[index] = '';
+      setPasscode(newPasscode);
+
       // Allow navigating to the previous input field when deleting digits
-      inputRefs[index - 1].current.focus();
+      if (index > 0) {
+        inputRefs[index - 1].current.focus();
+      }
     }
   };
 
+  const handlePaste = (e) => {
+    e.preventDefault();
+    const pasteData = e.clipboardData.getData('text/plain').trim();
+    const pasteValues = pasteData.split('').filter((char) => /^\d$/.test(char)).slice(0, 4);
+
+    const newPasscode = [...passcode];
+    pasteValues.forEach((value, index) => {
+      newPasscode[index] = value;
+    });
+
+    setPasscode(newPasscode);
+    onSubmit(newPasscode.join(''));
+
+    inputRefs[Math.min(pasteValues.length - 1, 3)].current.focus();
+  };
+
   return (
-    <div
-    style={{width:'100%', display:'flex', gap:'20px'}}
-    >
+    <div style={{ width: '100%', display: 'flex', gap: '20px' }}>
       {passcode.map((digit, index) => (
         <input
           key={index}
@@ -35,10 +117,10 @@ const PasscodeInput = ({ onSubmit }) => {
           value={digit}
           maxLength="1"
           onChange={(e) => handleChange(index, e.target.value)}
+          onPaste={handlePaste}
           ref={inputRefs[index]}
-          style={{height:'64px', padding:'8px 16px', width:'75px', borderRadius:'8px' }}
+          style={{ height: '64px', padding: '8px 16px', width: '75px', borderRadius: '8px' }}
           className={`${styles.pinForm} ${styles[theme]}`}
-
         />
       ))}
     </div>
@@ -46,9 +128,7 @@ const PasscodeInput = ({ onSubmit }) => {
 };
 
 PasscodeInput.propTypes = {
-    onSubmit: PropTypes.func.isRequired,
-  };
+  onSubmit: PropTypes.func.isRequired,
+};
 
 export default PasscodeInput;
-
-
