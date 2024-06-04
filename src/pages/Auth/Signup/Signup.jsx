66ea6fb783187556f9f83dc8a33d99a0015f0cf1
tomiwadotJS <victorgbayedev@gmail.com>
2024-06-04 12:37:04 +0100

    new signup look

--- src/pages/Auth/Signup/Signup.jsx
+++ src/pages/Auth/Signup/Signup.jsx
@@ -7,6 +7,10 @@ import { useState, useEffect } from 'react'
 import { useDispatch, useSelector } from "react-redux"
 // import { store } from "../../../store"
 import { registerUser } from "../../../features/user/userSlice"
+import SignupIcon from '../../../assets/SignupIcon.svg'
+import bgdesign from '../../../assets/bgdesign.svg'
+import bgdesignleft from '../../../assets/bgdesignleft.svg'
+
 
 const initialState = {
   firstname: '',
@@ -48,11 +52,12 @@ const Signup = () => {
   
   return (
     <div style={{background:theme, height:'100vh'}}>
-
+      <img src={bgdesignleft} style={{position: 'fixed', top: '50%', left: 0, transform: 'translateY(-50%)'}}/>
       <div className={styles.SignupContainer} >
           <h4>Sign Up</h4>
           <form onSubmit={onSubmit} className={styles.SignupInputContainer}>
               <Input
+              label="First name"
               type="string"
               placeholder='First name'
               style={{height:'48px'}}
@@ -61,40 +66,60 @@ const Signup = () => {
               name='firstname'
               />
               <Input
+              label="Last name"
               type="string"
-              placeholder='Last name'
+              placeholder='Type last name here'
               style={{height:'48px'}}
               value={signupValues.lastname}
               onChange={handleChange}
               name='lastname'
               />
               <Input
+              label="Email"
               type="email"
-              placeholder='Email'
+              placeholder='Type email here'
               style={{height:'48px'}}
               value={signupValues.email}
               onChange={handleChange}
               name='email'
               />
               <Input
+              label="Password"
               type="password"
-              placeholder="Password"
+              placeholder="Type password here"
               style={{height:'48px'}}
               value={signupValues.password}
               onChange={handleChange}
               name='password'
               />
-          <Button
-          label={isLoading ? 'Signing up...' : 'Sign Up'}
-          style={{height:'46px', backgroundColor: isLoading && 'grey' }}
-          type='submit'
-          disabled={isLoading}
-         
-          />
+              <Input
+              label="Re -enter password"
+              type="password"
+              placeholder="Type password here"
+              style={{height:'48px'}}
+              value={signupValues.password}
+              onChange={handleChange}
+              name='password'
+              />
+              <Button
+              label={isLoading ? (
+                <div style={{display:'flex',alignItems:'center', gap:'8px', justifyContent:'center'}}>
+                  <img src={SignupIcon} alt="Sign Up" /> Signing up...
+                </div>
+              ) : (
+                <div style={{display:'flex',alignItems:'center', gap:'8px', justifyContent:'center'}}>
+                  <img src={SignupIcon} alt="Sign Up" /> Sign Up
+                </div>
+              )}
+              style={{ height: '46px', backgroundColor: isLoading ? 'gray' : '/* Inherit or set desired color */' }}
+              type="submit"
+              disabled={isLoading}
+              />
           </form>
           <p>Already have an account? <Link to='/login'><span>Log in</span></Link></p>
 
       </div>
+      <img src={bgdesign} style={{position:'fixed', top: '50%', right: 0, transform: 'translateY(-50%)'}}/>
     </div>
   )
 }
